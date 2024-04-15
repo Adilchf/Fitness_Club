bf2a9f69ab2b05793f088c40c4f0dbe4f7872e2a
Adel <adelchellaf7@gmail.com>
2024-04-15 20:07:11 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import React from 'react'
-import './App.css'
-import { Route , Routes} from 'react-router-dom';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import { Box } from '@mui/material';
+
+import './App.css';
+
+import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
-import Home from './pages/Home';
+const App = () => (
+  <Box width="400px" sx={{ width: { xl: '1488px' } }} m="auto">
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      
+    </Routes>
+    <Footer />
+  </Box>
+);
 
-function App () {
-  return (
-    <Box width = '400px' sx={{ width: { xl: '1488px' } }} m="auto">
-        <Navbar />
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            
-        </Routes>
-        <Footer />
-  
-    </Box>
-  )
-}
-export default App
+export default App;
--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
 import HeroBannerImage from '../assets/images/banner.png';
 
-function HeroBanner()  {
-  return (
-    <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
     <Typography color="#FF2625" fontWeight="600" fontSize="26px">Fitness Club</Typography>
     <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
       Sweat, Smile <br />
@@ -22,6 +21,6 @@ function HeroBanner()  {
     </Typography>
     <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
   </Box>
-  )
-}
-export default HeroBanner
+);
+
+export default HeroBanner;
